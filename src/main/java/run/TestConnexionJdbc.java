a48763e154f9c65b16eab0ef02796344d6766548
Palliès <palliescie@gmail.com>
2021-11-03 15:00:17 +0100

    .properties

--- src/main/java/run/TestConnexionJdbc.java
+++ src/main/java/run/TestConnexionJdbc.java
@@ -1,14 +1,20 @@
+package run;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
+	private static ResourceBundle properties = ResourceBundle.getBundle("database");
 
-	private static final String DB_URL = "jdbc:mysql://localhost:3306/compta?useSSL=false";
-	private static final String DB_LOGIN = "root";
-	private static final String DB_PWD = "";
+	private static final String DB_URL = properties.getString("MYSQL_ADDON_HOST");
+	private static final String DB_LOGIN = properties.getString("MYSQL_ADDON_USER");
+	private static final String DB_PWD = properties.getString("MYSQL_ADDON_PASSWORD");
 
 	public static void main(String[] args) throws SQLException {
+
 
 		Connection connection = null;
 		try {
@@ -18,7 +24,7 @@ public class TestConnexionJdbc {
 		} catch (SQLException e) {
 			connection.close();
 		}
-	
+
 	}
 
 }
